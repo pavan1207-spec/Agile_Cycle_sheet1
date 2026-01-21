b0b3e9e34d6f24b6845935cb6d6964ed36a8528f
madhumithan2023-alt <madhumitha.n2023@vitstudent.ac.in>
2026-01-21 13:31:22 +0530

    marks module

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
+def calculate_grade(marks):
+    if marks >= 90:
+        return "A+"
+    elif marks >= 80:
+        return "A"
+    elif marks >= 70:
+        return "B"
+    elif marks >= 60:
+        return "C"
+    elif marks >= 50:
+        return "D"
+    else:
+        return "Fail"
 
-def main():
-    print("Login to the system")
-    username = input("Enter the username: ")
-    password = int(input("Enter the passcode: "))
 
-main()
+students = []
+
+n = int(input("Enter number of students: "))
+
+for i in range(n):
+    print(f"\nStudent {i + 1}")
+    name = input("Enter student name: ")
+    marks = int(input("Enter marks (0–100): "))
+
+    grade = calculate_grade(marks)
+    students.append((name, marks, grade))
+
+
+print("\n--- Student Marks & Grades ---")
+
+
+print(f"{'Name':<20}{'Marks':<10}{'Grade':<10}")
+print("-" * 45)
+
+
+for name, marks, grade in students:
+    print(f"{name:<20}{marks:<10}{grade:<10}")
