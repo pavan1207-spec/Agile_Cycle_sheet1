be0af360a556726d7e327b4044d6b8b7a5355b23
SURYA-CLG <surya.r2023c@vitstudent.ac.in>
2026-01-21 13:19:15 +0530

    login module

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+CREDS = {"ADMIN":1234,"SURYA":1003,"PAVAN":1207}
+
+def login(username,password):
+    if username in CREDS and CREDS[username] == password:
+        return True
+    return False
 
 def main():
     print("Login to the system")
     username = input("Enter the username: ")
     password = int(input("Enter the passcode: "))
-print("testing")
+    print(login(username,password))
 main()
